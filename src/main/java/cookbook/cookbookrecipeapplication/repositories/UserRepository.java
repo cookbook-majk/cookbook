caf8eacffd41c6f47bbf0b802dd227089bbe8796
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2023-03-02 15:10:47 -0600

    created User CRUD, Security Configuration, and Password Hashing method to registration functionality

---
+++ src/main/java/cookbook/cookbookrecipeapplication/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package cookbook.cookbookrecipeapplication.repositories;
+
+import cookbook.cookbookrecipeapplication.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
