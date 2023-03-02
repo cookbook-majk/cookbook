54e90ea3b9124944e5f3aac7b0ad726724fc5c14
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2023-03-02 11:05:26 -0600

    establishing project as spring boot

---
+++ src/main/java/cookbook/cookbookrecipeapplication/CookbookRecipeApplication.java
@@ -0,0 +1,13 @@
+package cookbook.cookbookrecipeapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookbookRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookbookRecipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cookbook/cookbookrecipeapplication/CookbookRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package cookbook.cookbookrecipeapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookbookRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
