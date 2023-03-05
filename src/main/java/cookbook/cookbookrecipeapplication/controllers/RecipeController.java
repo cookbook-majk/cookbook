fd522859303ffe0f672e85ca0e38d69c71a33f7b
Kaylee Morris <kaylee.christine.morris@gmail.com>
2023-03-04 23:22:07 -0600

    working on the profile page

--- src/main/java/cookbook/cookbookrecipeapplication/controllers/RecipeController.java
+++ src/main/java/cookbook/cookbookrecipeapplication/controllers/RecipeController.java
@@ -1,4 +1,48 @@
 package cookbook.cookbookrecipeapplication.controllers;
 
+import cookbook.cookbookrecipeapplication.models.Recipe;
+import cookbook.cookbookrecipeapplication.services.RecipeDaoService;
+import cookbook.cookbookrecipeapplication.services.UserDaoService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class RecipeController {
+    private UserDaoService userDao;
+    private RecipeDaoService recipeDao;
+
+    public RecipeController(UserDaoService userDao, RecipeDaoService recipeDao) {
+        this.userDao = userDao;
+        this.recipeDao = recipeDao;
+    }
+
+    //    // View a Recipe
+//    @GetMapping("/posts/{id}")
+//    public String viewPost(@PathVariable long id, Model model){
+//        model.addAttribute("post", postService.getPostById(id));
+//        return "posts/show";
+//    }
+//
+//    // Draft a Recipe
+//    @GetMapping("/recipe/create")
+//    public String draftRecipe(){
+//        return "/create-recipe";
+//    }
+//
+//    // Create a Recipe
+//    @PostMapping("/posts/create")
+//    public String createPost(@ModelAttribute Post post) {
+//        postService.savePost(post);
+//        emailService.sendTextEmail(post);
+//        return "redirect:/posts/" + post.getId();
+//    }
+//
+//    // Edit a Recipe
+//    @GetMapping("/posts/{id}/edit")
+//    public String editPost(@PathVariable long id, Model model){
+//        model.addAttribute("post", postService.getPostById(id));
+//        return "posts/edit";
+//    }
+
 }
