b878198736b3ad410a8ec803eeccb12866d25044
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2023-03-07 11:41:32 -0600

    added list for instrucitons/ingredients in prep for recipe creation

---
+++ src/main/java/cookbook/cookbookrecipeapplication/models/IngredientList.java
@@ -0,0 +1,23 @@
+package cookbook.cookbookrecipeapplication.models;
+
+import java.util.List;
+
+public class IngredientList {
+
+    private List<Ingredient> ingredients;
+
+    public IngredientList() {
+    }
+
+    public IngredientList(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+}
